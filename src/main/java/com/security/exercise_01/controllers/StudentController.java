8de0cb8cb7694de2d234d47a032523cd77c30c43
asiripr <asiripramodaya2000@gmail.com>
2024-09-13 13:27:30 +0530

    initialized security config class

--- src/main/java/com/security/exercise_01/controllers/StudentController.java
+++ src/main/java/com/security/exercise_01/controllers/StudentController.java
@@ -1,5 +1,33 @@
 package com.security.exercise_01.controllers;
 
-public class StudentController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import com.security.exercise_01.models.Student;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class StudentController {
+	public List<Student> students = new ArrayList<Student>(
+			List.of(new Student(1, "shan", 73), new Student(2, "sakuni", 80)));
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) {
+		students.add(student);
+		return student;
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
 }
