0aa0292aacf1ac759394afc477ec917ab291aa91
asiripr <asiripramodaya2000@gmail.com>
2024-09-16 14:28:21 +0530

    bcrypt secure

--- src/main/java/com/security/exercise_01/service/UserService.java
+++ src/main/java/com/security/exercise_01/service/UserService.java
@@ -1,6 +1,7 @@
 package com.security.exercise_01.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.security.exercise_01.models.Users;
@@ -12,7 +13,10 @@ public class UserService {
 	@Autowired
 	private UserRepo repo;
 	
+	private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
+	
 	public Users register(Users user) {
+		user.setPassword(encoder.encode(user.getPassword()));
 		return repo.save(user);
 	}
 }
