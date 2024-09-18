75c89de42f75085b9178d4afb69d162835e605ec
asiripr <asiripramodaya2000@gmail.com>
2024-09-18 11:37:19 +0530

    added jjwt api imple and jackson

--- src/main/java/com/security/exercise_01/config/SecurityConfig.java
+++ src/main/java/com/security/exercise_01/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.security.exercise_01.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,6 +38,13 @@ public class SecurityConfig {
 		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		provider.setUserDetailsService(userDetailsService);
 		return provider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+		
+		return config.getAuthenticationManager();
+		
 	}
 //	@Bean
 //	public UserDetailsService userDetailsService() {
