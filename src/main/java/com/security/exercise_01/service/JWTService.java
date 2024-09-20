73309b6193931622d68225830c939dcf1713db2f
asiripr <asiripramodaya2000@gmail.com>
2024-09-20 11:46:48 +0530

    jwt token

--- src/main/java/com/security/exercise_01/service/JWTService.java
+++ src/main/java/com/security/exercise_01/service/JWTService.java
@@ -1,10 +1,57 @@
 package com.security.exercise_01.service;
 
-public class JWTService {
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-	public String generateToken() {
-		// TODO Auto-generated method stub
-		return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.*;
+import io.jsonwebtoken.security.*;
+
+@Service
+public class JWTService {
+	private String secretKey = "";
+	
+	public JWTService() {
+		KeyGenerator keyGen;
+		try {
+			keyGen = KeyGenerator.getInstance("HmacSHA256");
+			SecretKey sk = keyGen.generateKey();
+			secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public String generateToken(String username) {
+		
+		Map<String, Object> claims = new HashMap<>();
+		
+		return Jwts.builder()
+				.claims()
+				.add(claims)
+				.subject(username)
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis()+ 60*60*30))
+				.and()
+				.signWith((java.security.Key) getKey())
+				.compact();
+		
+	}
+
+	private Key getKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+		return Keys.hmacShaKeyFor(keyBytes);
 	}
 
 }
--- src/main/java/com/security/exercise_01/service/UserService.java
+++ src/main/java/com/security/exercise_01/service/UserService.java
@@ -1,9 +1,9 @@
 package com.security.exercise_01.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.pulsar.PulsarProperties.Authentication;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,32 +12,33 @@ import com.security.exercise_01.repository.UserRepo;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	private UserRepo repo;
-	
-	@Autowired
-	AuthenticationManager authManager;
-	
-	@Autowired
-	private JWTService jwtService;
-	
-	private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
-	
-	public Users register(Users user) {
-		user.setPassword(encoder.encode(user.getPassword()));
-		return repo.save(user);
-	}
+    
+    @Autowired
+    private UserRepo repo;
+    
+    @Autowired
+    AuthenticationManager authManager;
+    
+    @Autowired
+    private JWTService jwtService;
+    
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+    
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
 
-	public String verify(Users user) {
-		Authentication authentication = (Authentication) authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-		
-		if (((org.springframework.security.core.Authentication) authentication).isAuthenticated()) {
-			return jwtService.generateToken();
-		}
-		else {
-			return "Failed";
-		}
-		
-	}
+    public String verify(Users user) {
+        // Use Spring Security's Authentication here
+        Authentication authentication = authManager.authenticate(
+            new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+        
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(user.getUsername());
+        } else {
+            return "Failed";
+        }
+    }
 }
